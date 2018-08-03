b645e0bd81186e34f45a355890c3625d08654752
Xiaopingliang <androidxiaoping@163.com>
2018-08-03 16:19:01 +0800

    third add commint

---
+++ src/test/java/DemeTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+public class DemeTest {
+    @Test
+    public void fun1() {
+        System.out.println("DemeTest ………………fun1");
+    }
+
+
+
+    @Test
+    public void fun2() {
+        System.out.println("DemeTest ………………fun2");
+    }
+
+
+
+    @Test
+    public void fun3() {
+        System.out.println("DemeTest ………………fun3");
+    }
+
+
+
+    @Test
+    public void fun4() {
+        System.out.println("DemeTest ………………fun4");
+    }
+}
